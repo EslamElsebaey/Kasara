a5aa6586245c760fc5a0757065bcd2f8166779d1
EslamElsebaey <eslamelsebaey.11@gmail.com>
2022-12-05 16:22:39 +0200

    edit

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ $(window).scroll(function(){
 })
 
 $(".toTop").click(function(){
-  $('html, body').animate({scrollTop:0}, '1000');
+  $('html, body').animate({scrollTop:0}, 150);
 }) 
 
 /************************************************************************ */
